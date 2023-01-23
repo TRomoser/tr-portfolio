78d3dfff6e9ba854a98710c05996b566a9e77f60
Taylor Romoser <TRomoser@gmail.com>
2023-01-22 19:26:21 -0800

    commit

---
+++ js/main.js
@@ -0,0 +1,125 @@
+// Change background header
+function scrollHeader() {
+  const header = document.getElementById('header');
+  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+// Services Modal
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalCloses = document.querySelectorAll('.services__modal-close');
+
+let modal = function(modalClick) {
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener('click', () => {
+    modal(i);
+  })
+})
+
+modalCloses.forEach((mc) => {
+  mc.addEventListener('click', () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove('active-modal')
+    })
+  })
+})
+
+// Mixitup filter portfolio
+// let mixerPortfolio = mixitup('.work_container', {
+//   selectors: {
+//       target: '.work_card'
+//   },
+//   animation: {
+//       duration: 300
+//   }
+// });
+
+// Link active work
+// const linkWork = document.querySelectorAll('.work__item');
+
+// function activeWork() {
+//   if(linkWork) {
+//     linkWork.forEach(l => l.classList.remove('active-work'));
+//     this.classList.add('active-work');
+//   }
+// }
+
+// linkWork.forEach(l => l.addEventListener('click', activeWork));
+
+// Swiper Testimonial
+// let swiperTestimonial = new Swiper('.testimonial__container', {
+//   spaceBetween: 24,
+//   grabCursor: true,
+//   loop: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     576: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//       spaceBetween: 48,
+//     },
+//   },
+// });
+
+// Scroll sections active link
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+                                          
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+// Light Dark Theme
+const themeButton = document.getElementById('theme-button');
+const lightTheme = 'light-theme';
+const iconTheme = 'bxs-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light theme
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme);
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the light / icon theme
+  document.body.classList.toggle(lightTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+})
+
+// Scroll Reveal Animation
